b53cd4d559c1fe88634ea7ed4cf322c4b4d2787f
Dhanush Muruganandan <59329600+legend123345@users.noreply.github.com>
2022-07-28 05:42:44 +0000

    program 8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,16 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+largest = None
+smallest = None
+l1=list()
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num1=int(num)
+    except:
+        print("Invalid input")
+    l1.append(num1)
+largest=max(l1)
+smallest=min(l1)
+print("Maximum is",largest)
+print("Minimum is",smallest)
