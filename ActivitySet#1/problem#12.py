8f856678d1553ebdf43c080f7a6b74e63c2d9c35
Dhanush Muruganandan <59329600+legend123345@users.noreply.github.com>
2022-07-28 05:47:33 +0000

    program 12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,23 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+filename = "dataset/mbox-short.txt"
+fname = input("Enter file:")
+if len(fname) < 1 : fname = "mbox-short.txt"
+fh = open(fname)
+senders1 = list()
+senders = dict()
+for line in fh:
+    line = line.split()
+    if len(line) < 3 or line[0] != 'From':
+        continue
+    senders1.append(line[1])
+
+for sender in senders1:
+    senders[sender] = senders.get(sender, 0) + 1
+
+maximum = None
+theone = None
+for sender, count in senders.items():
+    if maximum is None or count > maximum:
+        maximum = count
+        theone = sender
+
+print(theone, maximum)
