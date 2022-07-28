11e0f1920965a3e685233c22e7f97a04a97b4f58
Dhanush Muruganandan <59329600+legend123345@users.noreply.github.com>
2022-07-28 05:48:36 +0000

    program 13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,20 @@
-# Network Programming
-# https://www.py4e.com/lessons/network
+filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+hours = list()
+counts = dict()
+for line in handle:
+    line = line.split()
+    if len(line) < 3 or line[0] != 'From':
+        continue
+    time= line[5].split(':')
+    hours.append(time[0])
+
+for hour in hours:
+    counts[hour] = counts.get(hour, 0) + 1
+
+lst = sorted([(k,v) for k,v in counts.items()])
+
+for k,v in lst:
+    print(k,v)
