b399707bb17db45a2ef106ad50c27c682ed36599
Dhanush Muruganandan <59329600+legend123345@users.noreply.github.com>
2022-07-28 05:46:06 +0000

    program 11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,33 @@
-# Tuples
+filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+lst1=list()
+a=""
+for line in fh:
+    a=line.split()
+    lst+=a
+    for i in lst:
+        if i not in lst1:
+            lst1.append(i)
+        else:
+            continue
+lst1.sort()
+print(lst1)
+#8.5
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
 
-filename = "dataset/mbox-short.txt"
+fh = open(fname)
+count = 0
+for line in fh:
+    if line.startswith("From") and "From:" not in line:
+        text = line
+        c=text.split()
+        ext=c[1]
+        ext1=ext.strip()
+        print(ext1)
+        count+=1
+
+print("There were", count, "lines in the file with From as the first word"
