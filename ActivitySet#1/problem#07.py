3ba96ddb5ab1568f132dadbfe63a2cc6c50dcf2f
Dhanush Muruganandan <59329600+legend123345@users.noreply.github.com>
2022-07-28 05:41:43 +0000

    program 7

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,13 @@
-# Strings
-
-text = "X-DSPAM-Confidence:    0.8475"
+def computepay(h, r):
+  if h<=40 and h>=0:
+    p=h*r
+  if h>40:
+    h1=h-40
+    r1=1.5*r
+    p1=h1*r1
+    p=40*r+p1
+  return p
+hrs = float(input("Enter hours: "))
+rte = float(input("Enter rate per hour: "))
+pay = computepay(hrs, rte)
+print("Pay", pay)
